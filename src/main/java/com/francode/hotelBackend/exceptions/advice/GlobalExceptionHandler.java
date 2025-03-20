2c53d04cf3a40cb9a728cf545b003696fd93acea
francode-taype <francotaypedev@gmail.com>
2025-03-19 19:21:53 -0500

    feat: add exception handler for MethodArgumentNotValidException in exceptions folder

--- src/main/java/com/francode/hotelBackend/exceptions/advice/GlobalExceptionHandler.java
+++ src/main/java/com/francode/hotelBackend/exceptions/advice/GlobalExceptionHandler.java
@@ -7,10 +7,18 @@ import com.francode.hotelBackend.exceptions.custom.NotFoundException;
 import com.francode.hotelBackend.exceptions.custom.ValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.NoHandlerFoundException;
+
+import java.nio.file.AccessDeniedException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -41,6 +49,20 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ApiResponseDTO> handleNoRecordsException(NoRecordsException ex) {
         ApiResponseDTO response = new ApiResponseDTO(200, ex.getMessage());
         return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    // Manejar excepciones de validación
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponseDTO> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        List<String> errorMessages = ex.getBindingResult().getAllErrors().stream()
+                .map(error -> ((FieldError) error).getDefaultMessage())
+                .collect(Collectors.toList());
+
+        String errorMessage = String.join(", ", errorMessages);
+        ApiResponseDTO errorResponse = new ApiResponseDTO(HttpStatus.BAD_REQUEST.value(), errorMessage);
+
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
